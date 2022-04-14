599877bb9ee1beb426bfa9b52995f71b5fec9078
fischech123 <cfische2@stud.hs-offenburg.de>
2022-04-14 17:33:29 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Verkauf_Versand_Service';
+}
